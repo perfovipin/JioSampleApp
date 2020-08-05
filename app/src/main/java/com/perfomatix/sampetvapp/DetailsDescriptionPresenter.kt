88fd01143a308ac08cb528ba5204110c9198eca8
goutham <goutham.s@perfomatix.com>
2020-08-05 21:11:30 +0530

    button  implemented  in jio

--- app/src/main/java/com/perfomatix/sampetvapp/DetailsDescriptionPresenter.kt
+++ app/src/main/java/com/perfomatix/sampetvapp/DetailsDescriptionPresenter.kt
@@ -1,10 +1,22 @@
 package com.perfomatix.sampetvapp
 
-import androidx.leanback.widget.AbstractDetailsDescriptionPresenter
+import android.R.attr.button
+import android.content.Context
+import android.util.TypedValue
+import android.view.Gravity
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.leanback.widget.Presenter
+import com.perfomatix.leanback_jioext.Button
 
-class DetailsDescriptionPresenter : AbstractDetailsDescriptionPresenter() {
 
-    override fun onBindDescription(
+class DetailsDescriptionPresenter : Presenter() {
+
+private  var v: Button? = null
+    private  var mContext:Context ? = null
+
+    /*override fun onBindDescription(
             viewHolder: AbstractDetailsDescriptionPresenter.ViewHolder,
             item: Any) {
         val movie = item as Movie
@@ -12,5 +24,31 @@ class DetailsDescriptionPresenter : AbstractDetailsDescriptionPresenter() {
         viewHolder.title.text = movie.title
         viewHolder.subtitle.text = movie.studio
         viewHolder.body.text = movie.description
+    }*/
+
+    override fun onCreateViewHolder(parent: ViewGroup?): ViewHolder {
+
+        //
+
+        // create a new view
+       v =Button(parent?.context)
+        v?.background = null
+        v?.layoutParams = null
+        v?.gravity =Gravity.CENTER
+        mContext= parent?.context
+        return  ViewHolder(v)
+    }
+
+    override fun onBindViewHolder(viewHolder: ViewHolder?, item: Any?) {
+        val movie = item as Movie
+        v?.text ="PLAY"
+        v?.setOnClickListener {
+            Toast.makeText(mContext,"will be released soon ",Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+    override fun onUnbindViewHolder(viewHolder: ViewHolder?) {
+        v?.invalidate()
     }
 }
